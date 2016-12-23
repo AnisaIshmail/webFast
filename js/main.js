a699587d4419a886b7d1ea59afb14edc6c43ae76
Anisa Ishmail <anisa.ishmail@gmail.com>
2016-12-23 00:36:55 +0000

    Added the enter key functionality

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ function webFast(){
 		var url = $("#search-url").val();
 		window.location = "http://googleweblight.com/?lite_url=" + protocol + url;
 	});
+  	$("#search-url").on("keydown", function (e) {
+		if (e.keyCode === 13) { //checks whether the pressed key is "Enter"
+		var protocol = "http://"
+		var url = $("#search-url").val();
+		window.location = "http://googleweblight.com/?lite_url=" + protocol + url;
+		}
+    });
+	
 }
 
 $(webFast);
+
